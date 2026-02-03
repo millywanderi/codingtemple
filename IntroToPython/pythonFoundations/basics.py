3a466cf10f97e329b73baea60896577866b038a4
millywanderi <wanderinyambura5@gmail.com>
2026-02-03 12:27:55 +0300

    prompted user to input their age, name and color

--- IntroToPython/pythonFoundations/basics.py
+++ IntroToPython/pythonFoundations/basics.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python3
-# They are containers for storing data
 
-name = "Millicent"
-age = 31
-color = "beige"
+# Create a program that takes your name, age, and favorite color
+# as an input and prints them out.
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+color = input("Enter your favorite color: ")
 
 print(f"{name}, {age}, {color}")
 
-# Control Structures
+# Build a program that asks the user to input their exam score and then prints a message.
+# Outputs should be as follows:
+
+#    "Excellent" if the score is greater than 90.
+#    "Good" if the score is between 70 and 90.
+#    "Needs Improvement" if the score is below 70.
+
 score = int(input("Enter your exam score: "))
-if score > 90:
+if (score > 90):
     print("Excellent")
-elif score > 70 and score <= 90:
+elif (score < 90 and score > 70):
     print("Good")
 else:
     print("Needs Improvement")
-# print(f"Your exam score is: {score}")
