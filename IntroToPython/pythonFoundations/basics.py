0dfa070d09211e90ca0a7fe1014bf611c68dcfaf
millywanderi <wanderinyambura5@gmail.com>
2026-02-04 14:55:42 +0300

    Changed the comments to make it more appealing

--- IntroToPython/pythonFoundations/basics.py
+++ IntroToPython/pythonFoundations/basics.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python3
 
-# Create a program that takes your name, age, and favorite color
-# as an input and prints them out.
+"""
+Create a program that takes your name, age, and favorite color
+as an input and prints them out.
+"""
 name = input("Enter your name: ")
 age = input("Enter your age: ")
 color = input("Enter your favorite color: ")
 
 print(f"{name}, {age}, {color}")
 
-# Build a program that asks the user to input their exam score and then prints a message.
-# Outputs should be as follows:
+"""
+Build a program that asks the user to input their exam score and then prints a message.
+Outputs should be as follows:
 
-#    "Excellent" if the score is greater than 90.
-#    "Good" if the score is between 70 and 90.
-#    "Needs Improvement" if the score is below 70.
+* "Excellent" if the score is greater than 90.
+* "Good" if the score is between 70 and 90.
+* "Needs Improvement" if the score is below 70.
+"""
 
 score = int(input("Enter your exam score: "))
 if (score > 90):
