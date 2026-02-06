e83ba0a2857649c7bdfbaf229dea7195d88c8686
millywanderi <wanderinyambura5@gmail.com>
2026-02-06 14:01:13 +0300

    Added index/array string

--- IntroToPython/strings/string.py
+++ IntroToPython/strings/string.py
@@ -9,4 +9,11 @@ print(name) # print John
 greeting = 'Hello'
 print(greeting) # prints hello
 
+multi_line = '''My name is Kylie. I am 10 years old. I school at Good
+Testimony Schools. I am at Grade 5.'''
+print(multi_line)
 
+# strings index/array starts at 0
+name = "Kylie"
+print(name[0]) # prints k
+print(name[3]) # prints i
