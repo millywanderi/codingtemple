c96d10e062e2291c6e6c00f9c50714b88f3fac55
millywanderi <wanderinyambura5@gmail.com>
2026-02-11 15:17:56 +0300

    Added integer arithmetic operations

--- IntroToPython/integers/integers.py
+++ IntroToPython/integers/integers.py
@@ -5,4 +5,10 @@ a = 3 # positive integer
 b = -3 # negative integer
 c = 0 # zero integer
 
-# 
+# arithmetic
+x = 20
+y = 10
+print(x + y) # output is 20
+print(x - y) # output is 10
+print(x * y) # output is 200
+print(x / y) # output is 2.0 because it gives a float
