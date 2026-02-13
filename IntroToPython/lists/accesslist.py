b4e2badf8b0e70a29ad37af5f95e10bda302676b
millywanderi <wanderinyambura5@gmail.com>
2026-02-13 16:21:47 +0300

    Sliced list items

--- IntroToPython/lists/accesslist.py
+++ IntroToPython/lists/accesslist.py
@@ -6,4 +6,9 @@ print(fruits[1]) # output mango
 # Access negative index
 fruits = ["apple", "mango", "banana", "banana", "cherry", "orange"]
 print(fruits[-1]) # output orange
-print(fruits[-3]) # output banana, cherry, orange
+print(fruits[-3]) # output banana
+
+# slice a list
+list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+sublist1 = list1[1:4]
+print(sublist1) # output [2, 3, 4] 
