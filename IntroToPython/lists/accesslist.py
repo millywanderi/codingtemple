9169540051c273a464ab692aca43a99823dd5f0b
millywanderi <wanderinyambura5@gmail.com>
2026-02-13 16:14:21 +0300

    Added access item using negative index

--- IntroToPython/lists/accesslist.py
+++ IntroToPython/lists/accesslist.py
@@ -2,3 +2,8 @@
 
 fruits = ["apple", "mango", "banana", "banana", "cherry", "orange"]
 print(fruits[1]) # output mango
+
+# Access negative index
+fruits = ["apple", "mango", "banana", "banana", "cherry", "orange"]
+print(fruits[-1]) # output orange
+print(fruits[-3]) # output banana, cherry, orange
