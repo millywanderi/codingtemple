ddefd0dd6523ddb3956ba78e5f41c371d251b0ee
millywanderi <wanderinyambura5@gmail.com>
2026-02-13 16:27:14 +0300

    Sliced list items [:4]

--- IntroToPython/lists/accesslist.py
+++ IntroToPython/lists/accesslist.py
@@ -11,4 +11,6 @@ print(fruits[-3]) # output banana
 # slice a list
 list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 sublist1 = list1[1:4]
-print(sublist1) # output [2, 3, 4] 
+print(sublist1) # output [2, 3, 4]
+print(list1[:4]) # [1, 2, 3, 4]
+
