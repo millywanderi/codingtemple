79a86cf6bef689098248e1050ad870de9aa2d6da
millywanderi <wanderinyambura5@gmail.com>
2026-02-16 16:01:50 +0300

    Used insert() to add a name at the second index

--- IntroToPython/lists/addList.py
+++ IntroToPython/lists/addList.py
@@ -4,3 +4,8 @@
 names = ["Steve", "Millie", "Kylie"]
 names.append("Lyle")
 print(names)
+
+# use insert() to add an item at the specific index
+names = ["Steve", "Millie", "Kylie", "Lyle"]
+names.insert(2, "Kinsley")
+print(names)
