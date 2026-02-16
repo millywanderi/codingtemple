72e1e7c97fe632fca1274cff93104da4a0d3d4af
millywanderi <wanderinyambura5@gmail.com>
2026-02-16 16:26:45 +0300

    Use pop() to remove an item from specific index

--- IntroToPython/lists/removeList.py
+++ IntroToPython/lists/removeList.py
@@ -9,3 +9,8 @@ print(names)
 names = ["Steve", "Kylie", "Millie", "Kylie", "Lyle"]
 names.remove("Kylie")
 print(names)
+
+# remove item from specified index
+names = ["Steve", "Millie", "Kylie", "Lyle"]
+names.pop(1)
+print(names)
