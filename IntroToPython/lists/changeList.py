982fe1bf3dd1d929b8898c859da04c72bd83006e
millywanderi <wanderinyambura5@gmail.com>
2026-02-16 15:37:19 +0300

    Changed the second item of the list

--- IntroToPython/lists/changeList.py
+++ IntroToPython/lists/changeList.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
-#!/usr/bin/env python3
-
-
+# change the second item of the list
+names = ["Steve", "Millie", "Kylie", "Lyle"]
+names[1] = "Millicent"
+print(names) # prints ['Steve', 'Millicent', 'Kylie', 'Lyle']
