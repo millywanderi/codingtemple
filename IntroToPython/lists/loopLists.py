c229f5e63678372ea557332ef5d640d6da4eca06
millywanderi <wanderinyambura5@gmail.com>
2026-02-17 15:42:54 +0300

    Looping Using List Comprehension

--- IntroToPython/lists/loopLists.py
+++ IntroToPython/lists/loopLists.py
@@ -9,3 +9,14 @@ for x in fruits:
 fruits = ["mango", "banana", "apple"]
 for x in range(len(fruits)):
     print(fruits[x])
+
+# Print all items, using a while loop to go through all the index numbers
+fruits = ["mango", "banana", "apple"]
+x = 0
+while x < len(fruits):
+    print(fruits[x])
+    x = x + 1
+
+# Looping Using List Comprehension
+fruits = ["mango", "banana", "apple"]
+[print(x) for x in fruits]
