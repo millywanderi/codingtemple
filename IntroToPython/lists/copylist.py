9e2556c720339c966fb232252ff5ebef9246836b
millywanderi <wanderinyambura5@gmail.com>
2026-02-18 15:12:49 +0300

    Copied a list using list()

--- IntroToPython/lists/copylist.py
+++ IntroToPython/lists/copylist.py
@@ -4,3 +4,8 @@
 names = ["Steve", "Millie", "Kylie", "Lyle"]
 mynames = names.copy()
 print(mynames)
+
+# make a copy using list()
+names = ["Steve", "Millie", "Kylie", "Lyle"]
+mynames = list(names)
+print(mynames)
