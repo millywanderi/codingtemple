63c062bfccf2ca301c3cbefbd4dd6a2717d80d49
millywanderi <wanderinyambura5@gmail.com>
2026-02-18 14:57:31 +0300

    Sort the list based on how close the number is to 50

--- IntroToPython/lists/sortlist.py
+++ IntroToPython/lists/sortlist.py
@@ -18,3 +18,11 @@ print(names)
 numbers = [23, 45, -7, 3.14]
 numbers.sort(reverse = True)
 print(numbers)
+
+# Sort the list based on how close the number is to 50
+def myfunc(x):
+    return abs(x - 50)
+
+numbers = [75, 50, 22, 83, 100]
+numbers.sort(key = myfunc)
+print(numbers)
