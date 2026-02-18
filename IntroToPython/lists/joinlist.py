b813ed212cca9938708ee5beea492afe80796d9d
millywanderi <wanderinyambura5@gmail.com>
2026-02-18 15:21:36 +0300

    used append to join list

--- IntroToPython/lists/joinlist.py
+++ IntroToPython/lists/joinlist.py
@@ -5,3 +5,11 @@ list1 = [20, 21, 22]
 list2 = [23, 24, 25]
 list3 = list1 + list2
 print(list3)
+
+# join list by appending
+list1 = ["a", "b", "c"]
+list2 = [1, 2, 3]
+
+for x in list2:
+    list1.append(x)
+print(list1)
