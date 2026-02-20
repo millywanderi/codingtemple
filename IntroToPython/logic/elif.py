3f5de501d579f54dd38d7408feb5fa790c92e107
millywanderi <wanderinyambura5@gmail.com>
2026-02-20 15:47:52 +0300

    Added a multiple elif statements

--- IntroToPython/logic/elif.py
+++ IntroToPython/logic/elif.py
@@ -8,3 +8,16 @@ if y > x:
     print("y is greater than x")
 elif y == x:
     print("y is equal to x")
+
+# multiple statements
+score = 75
+
+if score >= 90:
+    print("Grade: A")
+elif score >= 80:
+    print("Grade: B")
+elif score >= 70:
+    print("Grade: C")
+elif score >= 60:
+    print("Grade: D")
+
