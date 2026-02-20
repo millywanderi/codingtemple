5400985841c7c6173eba1209488a4750b1129d5c
millywanderi <wanderinyambura5@gmail.com>
2026-02-20 16:06:09 +0300

    Added a simple else execution without elif to excute that x is not an even number

--- IntroToPython/logic/else.py
+++ IntroToPython/logic/else.py
@@ -18,3 +18,11 @@ if b > a:
     print("b is greater than a")
 else:
     print("b is not greater than a")
+
+# else provides default when other condions are not met
+x = 9
+
+if x % 2 == 0:
+    print("x is an even number")
+else:
+    print("x is not an even number")
