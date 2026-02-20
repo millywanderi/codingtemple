a6a360eba00b7d25a58855f7710ab96b15a66c88
millywanderi <wanderinyambura5@gmail.com>
2026-02-20 15:35:41 +0300

    Added multiple statements on a block

--- IntroToPython/logic/logic.py
+++ IntroToPython/logic/logic.py
@@ -7,3 +7,14 @@ elif x == 0:
     print("x is zero")
 else:
     print("x is negative")
+
+# if statement
+n = 20
+if n > 0:
+    print("n is positive")
+
+age = 20
+if age >= 18:
+    print("You are an adult")
+    print("You can vote")
+    print("You have full legal rights")
