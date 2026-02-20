879e0f3e5d5ded0645ffb120ae240ccfeafd4a04
millywanderi <wanderinyambura5@gmail.com>
2026-02-20 15:59:18 +0300

    Added a simple else execution

---
+++ IntroToPython/logic/else.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# it catches anything which isn't caught by the preceding conditions
+a = 30
+b = 20
+
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a is equal to b")
+else:
+    print("a is greater than b")
