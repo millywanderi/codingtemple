3edade3614d1eee792f62ad0e220f8f7a9031836
millywanderi <wanderinyambura5@gmail.com>
2026-02-20 15:53:24 +0300

    Added a multiple elif statements to get the forth day of the week

--- IntroToPython/logic/elif.py
+++ IntroToPython/logic/elif.py
@@ -21,3 +21,20 @@ elif score >= 70:
 elif score >= 60:
     print("Grade: D")
 
+# day checker
+day = 4
+
+if day == 1:
+    print("Monday")
+elif day == 2:
+    print("Tuesday")
+elif day == 3:
+    print("Wednesday")
+elif day == 4:
+    print("Thursday")
+elif day == 5:
+    print("Friday")
+elif day == 6:
+    print("Saturday")
+elif day == 7:
+    print("Sunday")
