eb66b6f6f397de20257a487d0b8439a2a7224192
millywanderi <wanderinyambura5@gmail.com>
2026-02-21 18:39:22 +0300

    Used or and no to test if the condition is true

--- IntroToPython/logic/logicaloperators.py
+++ IntroToPython/logic/logicaloperators.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python3
 
-# Returns True if both statements are true
+# and - Returns True if both statements are true
 x = 30
 y = 20
 z = 10
 if x > y and z < x:
     print("Both conditions are true")
+
+# or Returns True if one of the statements is true
+x = 30
+y = 20
+z = 10
+if x > y or x < z:
+    print("At least one condition is true")
+
+# not - Reverses the result, returns False if the result is true
+x = 30
+y = 200
+if not x > y:
+    print("x is not greater than y")
+
+
