91a30e813d1e63e23ec161b684228130b4f30b42
millywanderi <wanderinyambura5@gmail.com>
2026-02-21 18:19:53 +0300

    Added short hand if or else by assigning a variable

--- IntroToPython/logic/shorthand.py
+++ IntroToPython/logic/shorthand.py
@@ -9,3 +9,9 @@ if x > y: print("x is greater than y")
 x = 30
 y = 20
 print("X") if x > y else print("Y")
+
+# assign a value using short hand
+x = 30
+y = 20
+bigger = x if x > y else y
+print("Bigger is", bigger)
