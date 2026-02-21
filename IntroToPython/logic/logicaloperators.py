c3763148b82ea4c6f26e6d77c00c0e3c58e2adb8
millywanderi <wanderinyambura5@gmail.com>
2026-02-21 18:43:11 +0300

    Combining Multiple Operators

--- IntroToPython/logic/logicaloperators.py
+++ IntroToPython/logic/logicaloperators.py
@@ -20,4 +20,9 @@ y = 200
 if not x > y:
     print("x is not greater than y")
 
-
+# Combining Multiple Operators
+age = 25
+is_student = False
+has_discount_code = True
+if (age < 18 or age > 65) and not is_student or has_discount_code:
+    print("Discount applies!")
