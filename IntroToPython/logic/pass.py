d5cc8075198d8a39d76e76677569a62bd7526062
millywanderi <wanderinyambura5@gmail.com>
2026-02-23 16:54:23 +0300

    pass with Multiple Conditions

--- IntroToPython/logic/pass.py
+++ IntroToPython/logic/pass.py
@@ -6,3 +6,13 @@ if age < 18:
     pass
 else:
     print("Access granted")
+
+# pass with Multiple Conditions
+value = 50
+
+if value < 0:
+    print("Negative value")
+elif value == 0:
+    pass
+else:
+    print("Positive value")
