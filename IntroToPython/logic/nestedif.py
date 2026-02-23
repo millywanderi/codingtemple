c323f808dc2fe4b8ccd109ae4d1b12bf7c2a3fb3
millywanderi <wanderinyambura5@gmail.com>
2026-02-23 16:05:48 +0300

    Added a simple nested if requesting user to input number

--- IntroToPython/logic/nestedif.py
+++ IntroToPython/logic/nestedif.py
@@ -8,3 +8,13 @@ if num > 0:
         print("The number is postive and odd")
 else:
     print("The number is negative")
+
+x = int(input("Enter a number between 0 and 20:", ))
+if x > 10:
+    print("Is above 10.")
+    if x > 20:
+        print("Is above 20.")
+    else:
+        print("but not above 100")
+else:
+    print("Is negative")
