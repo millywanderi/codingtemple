3a368289fd9a9cd65cf6ab49757934def73d85b3
millywanderi <wanderinyambura5@gmail.com>
2026-02-23 16:00:36 +0300

    Added a simple nested if

---
+++ IntroToPython/logic/nestedif.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+num = 17
+if num > 0:
+    if num % 2 == 0:
+        print("The number is positive and even")
+    else:
+        print("The number is postive and odd")
+else:
+    print("The number is negative")
