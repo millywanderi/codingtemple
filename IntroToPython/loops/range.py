a977dc4773b592be5740009a99107435f3749f58
millywanderi <wanderinyambura5@gmail.com>
2026-02-24 15:40:04 +0300

    Iterate a list using range() and len()

--- IntroToPython/loops/range.py
+++ IntroToPython/loops/range.py
@@ -15,3 +15,8 @@ for i in range(2, 30, 3):
 # Iterate a list
 for i in list(range(5, 10)):
     print(i)
+
+# Iterate using len() and range()
+fruits = ["apple", "banana", "mango"]
+for i in range(len(fruits)):
+    print(i, fruits[i])
