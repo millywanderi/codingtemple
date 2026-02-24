75c418ed4b3dd62f26616bb7b1f137fdfdf8cde3
millywanderi <wanderinyambura5@gmail.com>
2026-02-24 15:54:47 +0300

    break before the identified item

--- IntroToPython/loops/breakcontinue.py
+++ IntroToPython/loops/breakcontinue.py
@@ -6,3 +6,10 @@ for i in fruits:
     print(i)
     if i == "banana":
         break
+
+# break before the identified item
+fruits = ["apple", "banana", "mango"]
+for i in fruits:
+    if i == "banana":
+        break
+    print(i)
