8e4f29a37a7430563d1b1f4d866b90327d1cff0e
millywanderi <wanderinyambura5@gmail.com>
2026-02-25 16:34:17 +0300

    With the break statement we can stop the loop even if the while condition is true

--- IntroToPython/loops/whileloop.py
+++ IntroToPython/loops/whileloop.py
@@ -5,3 +5,13 @@ x = 5
 while x < 10:
     print(x)
     x += 1
+
+# With the break statement we can stop the loop even if the while 
+# condition is true
+x = 5
+while x < 10:
+    print(x)
+    if x == 7:
+        break
+    print(x)
+    x += 1
