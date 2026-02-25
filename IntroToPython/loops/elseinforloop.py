07083fe41ae2db2a7407e3935011ffa01df83cc2
millywanderi <wanderinyambura5@gmail.com>
2026-02-25 16:11:43 +0300

    searches for prime numbers

--- IntroToPython/loops/elseinforloop.py
+++ IntroToPython/loops/elseinforloop.py
@@ -8,3 +8,12 @@ for x in range(6):
     print(x)
 else:
     print("Finally finished")
+
+# searches for prime numbers
+for n in range(2, 10):
+    for x in range(2, n):
+        if n % x == 0:
+            print(n, "equals", x, '*', n // x)
+            break
+    else:
+        print(n, "is a prime number")
