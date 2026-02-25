c147f63900263752b4086f422f5e3bdc2ce24596
millywanderi <wanderinyambura5@gmail.com>
2026-02-25 16:45:59 +0300

    loop and continue even if the number is 3

--- IntroToPython/loops/challenge.py
+++ IntroToPython/loops/challenge.py
@@ -10,3 +10,17 @@ for x in fruits:
     if x == "banana":
         break
     print(x)
+
+"""
+Create a variable i with the value 0
+Write a while loop that runs as long as i is less than 6
+Inside the loop: increment i by 1
+If i equals 3, use continue to skip that iteration
+print i
+"""
+i = 0
+while i < 6:
+    i += 1
+    if i == 3:
+        continue
+    print(i)
