42e24e24736cd57250ab892cd4be9566a0d1c18e
millywanderi <wanderinyambura5@gmail.com>
2026-02-26 20:13:48 +0300

    Created a function that greet and state name and hobby of a person

---
+++ IntroToPython/functions/challenge.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""
+Create a function introduce_yourself that takes a name and favorite hobby. 
+The function should print a greeting and mention the person's hobby.
+"""
+def introduce_yourself(name, hobby):
+    print(f"Hello, {name}! Correct me if I'm wrong, but your favorite hobby is {hobby}.")
+introduce_yourself("Kylie", "dancing")
+introduce_yourself("Lyle", "swimming")
