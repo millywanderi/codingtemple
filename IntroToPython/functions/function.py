cf278340740e2a8de3d2e51d2527e148fde14b82
millywanderi <wanderinyambura5@gmail.com>
2026-02-26 19:17:59 +0300

    A code without a function

--- IntroToPython/functions/function.py
+++ IntroToPython/functions/function.py
@@ -4,3 +4,16 @@
 def greet():
     print("Hello, welcome to my world!")
 greet()
+
+# Why use functions. Lets see code without and with a function
+temp1 = 77
+celsius1 = (temp1 - 32) * 5 / 9
+print(celsius1)
+
+temp2 = 95
+celsius2 = (temp2 - 32) * 5 / 9
+print(celsius2)
+
+temp3 = 50
+celsius3 = (temp3 - 32) * 5 / 9
+print(celsius3)
