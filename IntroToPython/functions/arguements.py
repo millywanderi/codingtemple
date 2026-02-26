85ffd0be245910c2dcf0e16935f01f53906a78ea
millywanderi <wanderinyambura5@gmail.com>
2026-02-26 19:50:32 +0300

    A function with one argument

--- IntroToPython/functions/arguements.py
+++ IntroToPython/functions/arguements.py
@@ -4,3 +4,10 @@
 def greet(name):
     print(f"Hello, {name}!")
 greet("Sam")
+
+# A function with one argument
+def my_name(first_name):
+    print(first_name + " Wanderi")
+my_name("Winnie")
+my_name("Irene")
+my_name("Millie")
