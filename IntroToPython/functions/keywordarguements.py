08ecc6429edaa3e7628c79b4a733a88aa8580e9d
millywanderi <wanderinyambura5@gmail.com>
2026-02-27 18:49:54 +0300

    Passing Different Data Types

--- IntroToPython/functions/keywordarguements.py
+++ IntroToPython/functions/keywordarguements.py
@@ -15,3 +15,10 @@ my_function("cat", "kiti")
 def my_function(animal, name, age):
     print("I have a", age, "year old", animal, "named", name)
 my_function("cat", name = "kiti", age = 2)
+
+# Passing Different Data Types
+def my_function(names):
+    for name in names:
+        print(name)
+my_names = ["Steve", "Millie", "Kylie", "Lyle"]
+my_function(my_names)
