e9dd5e3e093046cc7529fa4cb50f5535f01650c7
millywanderi <wanderinyambura5@gmail.com>
2026-02-27 18:41:03 +0300

    Positional Arguments

--- IntroToPython/functions/keywordarguements.py
+++ IntroToPython/functions/keywordarguements.py
@@ -4,3 +4,9 @@ def my_child(child, name):
     print("I have a", child)
     print("My", child + "'s name is", name)
 my_child(child = "daughter", name = "Kylie")
+
+# Positional Arguments
+def my_function(animal, name):
+    print("I have an", animal)
+    print("My", animal + "'s name is", name)
+my_function("cat", "kiti")
