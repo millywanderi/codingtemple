ea0a378f065843525d33a524794409593b2f1d5d
millywanderi <wanderinyambura5@gmail.com>
2026-03-02 18:17:33 +0300

    A function that calculates the sum of any number of values

--- IntroToPython/functions/argskwargs.py
+++ IntroToPython/functions/argskwargs.py
@@ -17,3 +17,13 @@ def my_function(greetings, *names):
     for name in names:
         print(greetings, name)
 my_function("Hello", "Kylie", "Lyle")
+
+# A function that calculates the sum of any number of values
+def my_function(*numbers):
+    total = 0
+    for num in numbers:
+        total += num
+    return total
+print(my_function(1, 2, 3))
+print(my_function(10, 20, 30, 40))
+print(my_function(5))
