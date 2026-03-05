e3474818fec5799f9e21cef28d4a30b9cc484238
millywanderi <wanderinyambura5@gmail.com>
2026-03-05 17:52:43 +0300

    make a function that always doubles the number you send in

--- IntroToPython/functions/lambda.py
+++ IntroToPython/functions/lambda.py
@@ -14,3 +14,9 @@ print(x(5))
 # Summarize argument a, b, and c and return the result
 x = lambda a, b, c: a + b + c
 print(x(1, 2, 3))
+
+# make a function that always doubles the number you send in
+def myfunc(n):
+    return lambda a : a * n
+mydoubler = myfunc(2)
+print(mydoubler(11))
