c3dc4778059f1c94895e47790658f418f008ea33
millywanderi <wanderinyambura5@gmail.com>
2026-03-06 15:45:09 +0300

    Identifying base case and recursive case

--- IntroToPython/functions/recursion.py
+++ IntroToPython/functions/recursion.py
@@ -8,3 +8,13 @@ def countdown(n):
         print(n)
         countdown( n - 1)
 countdown(5)
+
+# Identifying base case and recursive case
+def factorial(n):
+    # base case
+    if n == 0 or n == 1:
+        return 1
+    # recursive case
+    else:
+        return n * factorial(n - 1)
+print(factorial(5))
