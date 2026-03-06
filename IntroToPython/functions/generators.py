955ef79d05b21e9817ef1f5e867dac3afd2ff792
millywanderi <wanderinyambura5@gmail.com>
2026-03-06 16:22:42 +0300

    Generator that yields numbers

--- IntroToPython/functions/generators.py
+++ IntroToPython/functions/generators.py
@@ -9,3 +9,12 @@ def my_generator():
     yield 3
 for value in my_generator():
     print(value)
+
+# Generator that yields numbers
+def count_up_to(n):
+    count = 1
+    while count <= n:
+        yield count
+        count += 1
+for num in count_up_to(7):
+    print(num)
