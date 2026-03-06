ca522a740146f0fff98407d71a954832f421b20a
millywanderi <wanderinyambura5@gmail.com>
2026-03-06 16:48:46 +0300

    The send() method allows you to send a value to the generator

--- IntroToPython/functions/generators.py
+++ IntroToPython/functions/generators.py
@@ -64,3 +64,14 @@ def fibonacci():
 gen = fibonacci()
 for _ in range(100):
     print(next(gen))
+
+# The send() method allows you to send a value to the generator
+def echo_generator():
+    while True:
+        received = yield
+        print("Received", received)
+
+gen = echo_generator()
+next(gen)
+gen.send("Hello")
+gen.send("Tina")
