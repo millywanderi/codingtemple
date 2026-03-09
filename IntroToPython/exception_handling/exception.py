5ccb49a1de39ed7382df440f51b0a1ef8f85f869
millywanderi <wanderinyambura5@gmail.com>
2026-03-09 15:20:32 +0300

    Raise a TypeError if x is not an integer

--- IntroToPython/exception_handling/exception.py
+++ IntroToPython/exception_handling/exception.py
@@ -66,3 +66,8 @@ except:
 x = -1
 if x < 0:
     raise Exception("Sorry, no numbers below zero")
+
+# Raise a TypeError if x is not an integer
+x = "Millie"
+if not type(x) is int:
+    raise TypeError("Only integers allowed")
