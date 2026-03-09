20a9ba0903e4b75d5186921114aeeb92f206bef6
millywanderi <wanderinyambura5@gmail.com>
2026-03-09 15:24:31 +0300

    Challenge of a simple try..except...finally

--- IntroToPython/exception_handling/challenge.py
+++ IntroToPython/exception_handling/challenge.py
@@ -14,3 +14,15 @@ except ValueError:
     print("The value is not an integer or float")
 except ZeroDivisionError:
     print("You cannot divide the number by zero")
+
+"""
+Write a try block that tries to print a variable x (which is not defined)
+Write an except block that prints "An error occurred"
+Write a finally block that prints "Execution complete"
+"""
+try:
+    print(x)
+except:
+    print("An error occurred")
+finally:
+    print("Execution complete")
