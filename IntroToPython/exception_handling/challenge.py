623068b183e9a1cba89b8e9351d38e9b69992c7f
millywanderi <wanderinyambura5@gmail.com>
2026-03-09 15:38:37 +0300

    Atm challenge

--- IntroToPython/exception_handling/challenge.py
+++ IntroToPython/exception_handling/challenge.py
@@ -26,3 +26,30 @@ except:
     print("An error occurred")
 finally:
     print("Execution complete")
+
+"""
+Create a program that simulates an ATM withdrawal process. The program should:
+
+Allow the user to input an amount to withdraw.
+Raise an exception if the input is invalid (non-numeric or negative).
+Ensure that the withdrawal doesn’t exceed the account balance, 
+raising an appropriate exception.
+Always display the remaining balance, even if an error occurs.
+"""
+balance = 10000
+try:
+    amount = int(input("Enter the amount you want to withdraw: "))
+    if amount < 0:
+        raise ValueError("Withdrawal cannot be negative number")
+    if amount > balance:
+        raise Exception("Insufficient funds")
+    balance -= amount
+    print("Withdrawal successful")
+except ValueError:
+    print("Invalid! Enter a valid number")
+except Exception as e:
+    print("Error:", e)
+finally:
+    print("The remaining balance:", balance)
+
+
