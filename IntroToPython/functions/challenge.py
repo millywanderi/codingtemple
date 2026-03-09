0685f6c6c96fd0f962bc8fb4a8dd15a9d2fb697a
millywanderi <wanderinyambura5@gmail.com>
2026-03-09 13:56:29 +0300

    Test your understanding of creating functions, parameters, return values, and *args

--- IntroToPython/functions/challenge.py
+++ IntroToPython/functions/challenge.py
@@ -8,3 +8,13 @@ def introduce_yourself(name, hobby):
     print(f"Hello, {name}! Correct me if I'm wrong, but your favorite hobby is {hobby}.")
 introduce_yourself("Kylie", "dancing")
 introduce_yourself("Lyle", "swimming")
+
+"""
+Create a function called greet
+Add a parameter called name to the function
+Inside the function, print "Hello, " followed by the name parameter
+Call the function with the argument "Emil"
+"""
+def greet(name):
+    print("Hello", name)
+greet("Emil")
