087e6f697aeb9c03f255d7ac1ff548a901323ae4
millywanderi <wanderinyambura5@gmail.com>
2026-03-09 14:28:03 +0300

    Added exception ZeroDivisionError

---
+++ IntroToPython/exception_handling/exception.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+The try block lets you test a block of code for errors.
+
+The except block lets you handle the error.
+
+The else block lets you execute code when there is no error.
+
+The finally block lets you execute code, regardless of the result of 
+the try- and except blocks.
+"""
+try:
+    x = 10 / 0
+except ZeroDivisionError:
+    print("You cannot divide by zero")
