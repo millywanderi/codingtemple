aac4bd447d99e81bcff94d5d27a4564b4f53e40d
millywanderi <wanderinyambura5@gmail.com>
2026-03-10 14:56:54 +0300

    Practice Creating Sets

---
+++ AdvancedPython/sets/challenge.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+# Exercise 1: Practice Creating Sets
+Create a list of your favorite hobbies, making sure to repeat a few of them.
+Convert the list into a set to automatically remove the duplicates.
+Print both the original list and the set to compare.
+"""
+favoritehobbies = ["coding", "travelling", "reading", "coding", "watching"]
+hobbies = set(favoritehobbies)
+print(favoritehobbies)
+print(hobbies)
