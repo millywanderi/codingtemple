78ee114e4876d7a8d4f38b6c7a50dac2396b5f0c
millywanderi <wanderinyambura5@gmail.com>
2026-03-10 16:00:57 +0300

    Intersection: Returns only the items both sets have in common

--- AdvancedPython/sets/setsoperations.py
+++ AdvancedPython/sets/setsoperations.py
@@ -6,3 +6,10 @@ set2 = {"Kinsley", "Lyle"}
 
 grand_gather = set1.union(set2)
 print(grand_gather)
+
+# Intersection: Returns only the items both sets have in common
+set1 = {"Steve", "Millie", "Kylie", "Lyle"}
+set2 = {"Steve", "Millie","Jane", "Albert"}
+
+mutual_friends = set1.intersection(set2)
+print(mutual_friends)
