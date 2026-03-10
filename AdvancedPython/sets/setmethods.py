37175c578bb1e6913e98dd748ae78caffa8b6fd4
millywanderi <wanderinyambura5@gmail.com>
2026-03-10 15:26:17 +0300

    Subset and Superset Checks

--- AdvancedPython/sets/setmethods.py
+++ AdvancedPython/sets/setmethods.py
@@ -9,3 +9,10 @@ print("Sue" in names)
 names = {"Steve", "Millie", "Kylie", "Lyle"}
 names.add("Kinsley")
 print(names)
+
+# Subset and Superset Checks
+num1 = {5, 6, 7}
+num2 = {5, 6, 7, 8, 9}
+
+print(num1.issubset(num2))
+print(num2.issuperset(num1))
