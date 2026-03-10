6cf3f2c6744fee4858ab589a8b2ef184ebf9446a
millywanderi <wanderinyambura5@gmail.com>
2026-03-10 14:51:42 +0300

    Using sets to remove duplicates from other data structures.

--- AdvancedPython/sets/sets.py
+++ AdvancedPython/sets/sets.py
@@ -3,3 +3,8 @@
 # Creating a Set with Values
 new_set = {'one', 'two', 'three'}
 print(new_set)
+
+# Using sets to remove duplicates from other data structures.
+myfruits = ["apple", "apple", "banana", "mango"]
+fruits = set(myfruits)
+print(fruits)
