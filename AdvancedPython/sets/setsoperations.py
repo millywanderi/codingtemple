f4a2200b76f2aa244cfef33125ca5af4c9edb7c8
millywanderi <wanderinyambura5@gmail.com>
2026-03-10 15:36:24 +0300

    Union: Combines all unique items from two sets

---
+++ AdvancedPython/sets/setsoperations.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# Union: Combines all unique items from two sets
+set1 = {"Steve", "Millie", "Kylie"}
+set2 = {"Kinsley", "Lyle"}
+
+grand_gather = set1.union(set2)
+print(grand_gather)
