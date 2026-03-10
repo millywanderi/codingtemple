07bc27ada46ccc3862b3eb4e853841f509fee4e5
millywanderi <wanderinyambura5@gmail.com>
2026-03-10 15:16:01 +0300

    Adding Items to a Set

--- AdvancedPython/sets/setmethods.py
+++ AdvancedPython/sets/setmethods.py
@@ -4,3 +4,8 @@
 names = {"Steve", "Millie", "Kylie", "Lyle"}
 print("Kylie" in names)
 print("Sue" in names)
+
+# Adding Items to a Set
+names = {"Steve", "Millie", "Kylie", "Lyle"}
+names.add("Kinsley")
+print(names)
