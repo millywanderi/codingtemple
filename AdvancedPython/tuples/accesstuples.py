afda5ad0c2fce0950d81a0d6d888dc9380b02891
millywanderi <wanderinyambura5@gmail.com>
2026-03-11 13:34:55 +0300

    Check if apple is present in the tuple

--- AdvancedPython/tuples/accesstuples.py
+++ AdvancedPython/tuples/accesstuples.py
@@ -15,3 +15,8 @@ print(mytuple[2:5])
 # Range of Negative Indexes
 mytuple = (1, 2, 3, 4, 5, 6, 7, 8, 9)
 print(mytuple[-4:-1])
+
+# Check if "apple" is present in the tuple
+mytuple = ("apple", "banana", "mango")
+if "apple" in mytuple:
+    print("Yes, 'apple' is in the fruit tuple")
