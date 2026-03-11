b1ab23b59a910f692f8457471d72e68e9e127d62
millywanderi <wanderinyambura5@gmail.com>
2026-03-11 13:26:49 +0300

    Print the last item of the tuple

--- AdvancedPython/tuples/accesstuples.py
+++ AdvancedPython/tuples/accesstuples.py
@@ -3,3 +3,7 @@
 # Print the second item in the tuple
 mytuple = ("Steve", "Millie", "Kylie", "Lyle")
 print(mytuple[1])
+
+# Print the last item of the tuple
+mytuple = ("Steve", "Millie", "Kylie", "Lyle")
+print(mytuple[-1])
