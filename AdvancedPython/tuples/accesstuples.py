e165886c3ba874d3a930fb27c4f2316e4dad1a40
millywanderi <wanderinyambura5@gmail.com>
2026-03-11 13:29:25 +0300

    Return the third, fourth, and fifth item

--- AdvancedPython/tuples/accesstuples.py
+++ AdvancedPython/tuples/accesstuples.py
@@ -7,3 +7,7 @@ print(mytuple[1])
 # Print the last item of the tuple
 mytuple = ("Steve", "Millie", "Kylie", "Lyle")
 print(mytuple[-1])
+
+# Return the third, fourth, and fifth item
+mytuple = (1, 2, 3, 4, 5, 6, 7, 8, 9)
+print(mytuple[2:5])
