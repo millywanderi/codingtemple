bcf4017a8296e4fb2c7193cd02561e651623811b
millywanderi <wanderinyambura5@gmail.com>
2026-03-12 15:29:00 +0300

    Final Challenge: Tuple Mastery

--- AdvancedPython/tuples/challenge.py
+++ AdvancedPython/tuples/challenge.py
@@ -16,8 +16,8 @@ mytuple = (1, "Tom", True, 3.14)
 print("First element:", mytuple[0])
 print("Last element:", mytuple[-1])
 
-mytuple.add("Mary")
-print(mytuple)
+#mytuple.add("Mary")
+#print(mytuple)
 
 
 """
@@ -34,3 +34,33 @@ print(len(fruits))
 
 a, b, c = fruits
 print(b)
+
+
+"""
+Final Challenge: Tuple Mastery
+Now that you’ve explored the basics, let’s put everything together!
+
+Create a Tuple: Create a tuple with at least 6 elements. Feel free to
+mix different data types like integers, strings, and floats.
+Access and Print Elements: Access and print the third and fifth 
+elements using indexing.
+Slice the Tuple: Slice the tuple to extract elements from the second 
+to the fifth position
+Count Occurrences: Use the count() method to find how many times a 
+specific value appears in your tuple.
+Unpack the Tuple: Unpack the tuple into individual variables and 
+print them.
+Concatenate Tuples: Concatenate your tuple with another tuple and 
+print the new tuple.
+"""
+mytuple = (1.1, "Mary", True, 30, "Developer", True)
+print("Third element:", mytuple[2])
+print("Fifth element:", mytuple[4])
+print(mytuple[1:5])
+print(mytuple.count(True))
+
+a,b, c, d, e, f = mytuple
+print(a, b, c, d, e, f)
+
+newtuple = mytuple + (1.2, "Ann", 29, False)
+print(newtuple)
