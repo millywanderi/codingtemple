6e7c0913dce7191cb30042cdd90d8abad39a4a9f
millywanderi <wanderinyambura5@gmail.com>
2026-03-12 15:01:44 +0300

    Multiply the fruits tuple by 2

--- AdvancedPython/tuples/jointuples.py
+++ AdvancedPython/tuples/jointuples.py
@@ -5,3 +5,8 @@ tuple1 = ("a", "b", "c")
 tuple2 = (1, 2, 3, 4)
 
 print(tuple1 + tuple2)
+
+# Multiply the fruits tuple by 2
+fruits = ("apple", "banana", "mango")
+mytuple = fruits * 2
+print(mytuple)
