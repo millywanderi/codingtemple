1450751bb183e5f35f7313741e71f8bdf7ce0e5d
millywanderi <wanderinyambura5@gmail.com>
2026-03-12 15:08:12 +0300

    .index(): Returns the index of the first occurrence of a specified value

--- AdvancedPython/tuples/tuplemethods.py
+++ AdvancedPython/tuples/tuplemethods.py
@@ -4,3 +4,8 @@
 #the tuple
 numbers = (1, 2, 3, 2, 4, 2, 6, 2)
 print(numbers.count(2))
+
+# .index(): Returns the index of the first occurrence of a specified 
+#value
+numbers = (1, 2, 3, 2, 4, 2, 6, 2)
+print(numbers.index(6))
