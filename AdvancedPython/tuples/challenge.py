d876f71c51fae932188774abfb36091f4ea49b44
millywanderi <wanderinyambura5@gmail.com>
2026-03-12 14:09:44 +0300

    Accessed the first and last element in a tuple

---
+++ AdvancedPython/tuples/challenge.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""
+Tuple Exploration
+Let’s try a quick exercise to engage with tuples and understand their behavior!
+
+Create a Tuple: Create a tuple containing at least 4 different data
+types, such as an integer, string, float, and boolean.
+Access and Print Elements: Access and print the first and last
+elements of your tuple.
+Attempt to Modify the Tuple: Try modifying one element of the tuple
+and observe the error. This will help you understand the immutability
+of tuples.
+"""
+mytuple = (1, "Tom", True, 3.14)
+print("First element:", mytuple[0])
+print("Last element:", mytuple[-1])
