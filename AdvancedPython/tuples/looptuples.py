2ccdc50d791fd1c8204d310f0137de37baf3d78d
millywanderi <wanderinyambura5@gmail.com>
2026-03-12 14:56:17 +0300

    Print all items, using a while loop to go through all the index numbers

--- AdvancedPython/tuples/looptuples.py
+++ AdvancedPython/tuples/looptuples.py
@@ -9,3 +9,10 @@ for i in fruits:
 fruits = ("apple", "banana", "mango")
 for i in range(len(fruits)):
     print(fruits[i])
+
+# Print all items, using a while loop to go through all the index numbers
+fruits = ("apple", "banana", "mango")
+i = 0
+while i < len(fruits):
+    print(fruits[i])
+    i += 1
