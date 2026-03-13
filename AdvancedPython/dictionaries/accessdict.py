1b2c76b22f929611e7637666ac2be388c97b88f0
millywanderi <wanderinyambura5@gmail.com>
2026-03-13 16:50:21 +0300

    Add a new item to the original dictionary, and see that the values list gets updated as well

--- AdvancedPython/dictionaries/accessdict.py
+++ AdvancedPython/dictionaries/accessdict.py
@@ -36,3 +36,15 @@ car = {
     "year": 1964
 }
 print(car.values())
+
+# Add a new item to the original dictionary, and see that the values 
+#list gets updated as well
+car = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+x = car.values()
+print(x)
+car["color"] = "red"
+print(x)
