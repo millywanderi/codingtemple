0a0a587d45de265baf7e16d35ed966e160e158db
millywanderi <wanderinyambura5@gmail.com>
2026-03-13 16:40:25 +0300

    The .get() method is particularly useful for preventing errors when accessing keys🔑 that may or may not be in the dictionary.

--- AdvancedPython/dictionaries/accessdict.py
+++ AdvancedPython/dictionaries/accessdict.py
@@ -7,3 +7,12 @@ my_dict = {
     "school": "Precious Brooks"
 }
 print(my_dict["school"])
+
+# Get the value of the "model" key
+my_dict = {
+    "name": "Kylie",
+    "age": 10,
+    "school": "Precious Brooks"
+}
+print(my_dict.get("name"))
+print(my_dict.get("age"))
