b71ecdf4714d874b6a560aca8e371b847163e5ed
millywanderi <wanderinyambura5@gmail.com>
2026-03-14 12:59:25 +0300

    Print the name of child 2

--- AdvancedPython/dictionaries/nesteddictionaries.py
+++ AdvancedPython/dictionaries/nesteddictionaries.py
@@ -38,3 +38,20 @@ myfamily = {
     "child3": child3
 }
 print(myfamily)
+
+# Print the name of child 2
+myfamily = {
+    "child1": {
+    "name": "Steve",
+    "year": 2005
+    },
+    "child2": {
+        "name": "Kylie",
+        "year": 2015
+    },
+    "child3": {
+        "name": "Lyle",
+        "year": 2019
+    }
+}
+print(myfamily["child2"]["name"])
