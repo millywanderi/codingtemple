aa380d6e6dc4f6869d04eb1226bcb4b571138862
millywanderi <wanderinyambura5@gmail.com>
2026-03-14 11:54:11 +0300

    Adding an item to the dictionary is done by using a new index key and assigning a value to it

---
+++ AdvancedPython/dictionaries/additems.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Adding an item to the dictionary is done by using a new index key 
+#and assigning a value to it
+my_dict = {
+    "name": "Kylie",
+    "age": 10,
+    "school": "Precious Brooks"
+}
+my_dict["brother"] = "Lyle"
+print(my_dict)
