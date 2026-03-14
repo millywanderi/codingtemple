8126841cc688764ecb65d941c20ac42e339beca0
millywanderi <wanderinyambura5@gmail.com>
2026-03-14 12:46:36 +0300

    Create a dictionary that contain three dictionaries

---
+++ AdvancedPython/dictionaries/nesteddictionaries.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Create a dictionary that contain three dictionaries
+myfamily = {
+    "child1": {
+        "name": "Steve",
+        "year": 2005
+    },
+    "child2": {
+        "name":"Kylie",
+        "year": 2015
+    },
+    "child3": {
+        "name": "Lyle",
+        "year": 2019
+    }
+}
+print(myfamily)
