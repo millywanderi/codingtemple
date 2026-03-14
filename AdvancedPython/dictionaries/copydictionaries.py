b1524733749d318556435ba81370010b229c3337
millywanderi <wanderinyambura5@gmail.com>
2026-03-14 12:35:22 +0300

    Make a copy of a dictionary with the copy() method

---
+++ AdvancedPython/dictionaries/copydictionaries.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Make a copy of a dictionary with the copy() method
+mydict = {
+    "name": "Kylie",
+    "age": 10,
+    "city": "Nairobi"
+}
+thisdict = mydict.copy()
+print(thisdict)
