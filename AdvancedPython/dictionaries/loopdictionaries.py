d4ed46924e5f7d12520b66251f97df092897bfb4
millywanderi <wanderinyambura5@gmail.com>
2026-03-14 12:24:33 +0300

    Print all key names in the dictionary, one by one

---
+++ AdvancedPython/dictionaries/loopdictionaries.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Print all key names in the dictionary, one by one
+car = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1980
+}
+for x in car:
+    print(x)
