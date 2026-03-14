095c249187f6e3b1ede249864ecb2c32f39c8050
millywanderi <wanderinyambura5@gmail.com>
2026-03-14 11:47:07 +0300

    change the value of a specific item by referring to its key name

---
+++ AdvancedPython/dictionaries/changedict.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# change the value of a specific item by referring to its key name
+my_dict = {
+    "name": "Kylie",
+    "age": 10,
+    "school": "Precious Brooks"
+}
+my_dict["school"] = "Karamani Baptist"
+print(my_dict)
