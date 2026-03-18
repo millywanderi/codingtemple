8d6a066c51abe52f75cab643e2c11f0c5636cc10
millywanderi <wanderinyambura5@gmail.com>
2026-03-18 14:46:53 +0300

    Split the string only at the first occurrence

--- AdvancedPython/regEx/splitregex.py
+++ AdvancedPython/regEx/splitregex.py
@@ -7,6 +7,10 @@ txt = "The rain in Spain"
 x = re.split(r"\s", txt)
 print(x)
 
+#Split the string only at the first occurrencex
+x = re.split(r"\s", txt, 1)
+print(x)
+
 """
 split a string based on various delimiters such as commas, semicolons
 , spaces, periods, and hyphens.split a string based on various 
