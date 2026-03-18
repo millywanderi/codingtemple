041cafddc866dbc426659b04be32b55edf273b21
millywanderi <wanderinyambura5@gmail.com>
2026-03-18 15:32:53 +0300

    The regular expression looks for any words that starts with an upper case S

--- AdvancedPython/regEx/matchregex.py
+++ AdvancedPython/regEx/matchregex.py
@@ -6,3 +6,8 @@ import re
 txt = "The rain in Spain"
 x = re.search(r"ai", txt)
 print(x)
+
+# The regular expression looks for any words that starts with an 
+#upper case "S"
+x = re.search(r"\bS\w+", txt)
+print(x.span())
