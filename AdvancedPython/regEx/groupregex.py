40d306eb44d4e373df373b83d33d5e24eb3a4173
millywanderi <wanderinyambura5@gmail.com>
2026-03-18 15:53:53 +0300

    Capturing Parts of a Phone Number

--- AdvancedPython/regEx/groupregex.py
+++ AdvancedPython/regEx/groupregex.py
@@ -7,3 +7,11 @@ import re
 txt = "The rain in Spain"
 x = re.search(r"\bS\w+", txt)
 print(x.group())
+
+# Capturing Parts of a Phone Number
+number = "123-456"
+pattern = r"(\d+)-(\d+)"
+thematch = re.search(pattern, number)
+if thematch:
+    print(f"Group 1: {thematch.group(1)}")
+    print(f"Group 2: {thematch.group(2)}")
