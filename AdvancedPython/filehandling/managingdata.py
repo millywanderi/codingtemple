7dd04a1f03233094a03a246412d77c6e914944ed
millywanderi <wanderinyambura5@gmail.com>
2026-03-19 17:20:23 +0300

    Function to add a show to our shows list in dictionary format, and write it to our file with the write_show function

--- AdvancedPython/filehandling/managingdata.py
+++ AdvancedPython/filehandling/managingdata.py
@@ -8,3 +8,12 @@ def write_show(shows):
     with open("show_list.txt", "w") as file:
         for show in shows:
             file.write(f"{show['Title']}-:-{show['Platform']}-:-{show['Genre']}\n")
+
+# Function to add a show to our shows list in dictionary format, and 
+#write it to our file with the write_show function
+def add_show(shows):
+    title = input("What is the title of the show? ")
+    platform = input("Where can we watch it? ")
+    genre = input("What is the genre? ")
+    shows.append({'Title': title, 'Platform': platform, 'Genre': genre})
+    write_show(shows)
