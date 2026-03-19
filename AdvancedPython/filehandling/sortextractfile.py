33494f3d707da3758c1ca9292f5d850cc0981076
millywanderi <wanderinyambura5@gmail.com>
2026-03-19 16:45:01 +0300

    Extract list when handling a file

--- AdvancedPython/filehandling/sortextractfile.py
+++ AdvancedPython/filehandling/sortextractfile.py
@@ -6,3 +6,11 @@ flowers = ["Wysteria", "Sunflowers", "Orchids", "Marigolds"]
 with open("garden.txt", "w") as file:
     for flower in flowers:
         file.write(flower + "\n")
+
+# Extract list
+flowers = []
+
+with open("garden.txt", "r") as file:
+    for line in file:
+        flowers.append(line.strip())
+print(flowers)
