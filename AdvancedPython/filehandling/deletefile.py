e05891f56ece3852155604dbb4983c27f07598df
millywanderi <wanderinyambura5@gmail.com>
2026-03-19 16:30:27 +0300

    check if the file exists

--- AdvancedPython/filehandling/deletefile.py
+++ AdvancedPython/filehandling/deletefile.py
@@ -4,3 +4,9 @@ import os
 
 # Remove the file "call.txt"
 os.remove("call.txt")
+
+# Check if file exists, then delete it
+if os.path.exists("call.txt"):
+    os.remove("call.txt")
+else:
+    print("The file does not exist")
