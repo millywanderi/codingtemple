834a9d3225aa8264f4d7e4d5bf29bd9f7fd8b5d2
millywanderi <wanderinyambura5@gmail.com>
2026-03-20 15:55:06 +0300

    Create three objects from the MyClass class

--- AdvancedPython/oopfundamentals/classobject.py
+++ AdvancedPython/oopfundamentals/classobject.py
@@ -11,3 +11,12 @@ class myClass:
     x = 5
 pp1 = myClass()
 print(pp1.x)
+
+# Create three objects from the MyClass class
+pp2 = myClass()
+pp3 = myClass()
+pp4 = myClass()
+
+print(pp2.x)
+print(pp3.x)
+print(pp4.x)
