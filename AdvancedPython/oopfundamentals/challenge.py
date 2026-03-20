7dcf1d22a7cba0d353dd9465a596f320d66e07f7
millywanderi <wanderinyambura5@gmail.com>
2026-03-20 16:34:41 +0300

    Test my understanding of creating classes, __init__, properties, methods, and objects

--- AdvancedPython/oopfundamentals/challenge.py
+++ AdvancedPython/oopfundamentals/challenge.py
@@ -15,3 +15,19 @@ class Person:
         print("Hello, my name is " + self.name)
 p1 = Person("John", 36)
 p1.greet()
+
+"""
+Create a class called Dog
+Add an __init__ method with parameters name and age, and store them as properties using self
+Add a method called bark that prints the dog's name followed by " says Woof!"
+Create an object d1 of the Dog class with name "Buddy" and age 3
+Call the bark method on d1
+"""
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def bark(self):
+        print(self.name + " saya Woof!")
+d1 = Dog("Buddy", 3)
+d1.bark()
