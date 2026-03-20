dd536758e831bfe0f98a60f1f5be871274fedf14
millywanderi <wanderinyambura5@gmail.com>
2026-03-20 17:23:27 +0300

    Test my understanding of the self parameter by completing a small coding challenge

--- AdvancedPython/oopfundamentals/challenge.py
+++ AdvancedPython/oopfundamentals/challenge.py
@@ -31,3 +31,18 @@ class Dog:
         print(self.name + " saya Woof!")
 d1 = Dog("Buddy", 3)
 d1.bark()
+
+"""
+Create a class called Car
+Add an __init__ method with a brand parameter, and store it as a property
+Add a method called show that prints the brand
+Create an object c1 of the Car class with brand "Ford"
+Call the show method on c1
+"""
+class Car:
+    def __init__(self, brand):
+        self.brand = brand
+    def show(self):
+        print(self.brand)
+c1 = Car("Ford")
+c1.show()
