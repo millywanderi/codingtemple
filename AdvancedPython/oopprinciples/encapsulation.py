60e7ab16f820fb242192074303f8712ea1ccab65
millywanderi <wanderinyambura5@gmail.com>
2026-03-21 15:11:40 +0300

    Example of Protected Attributes (BankAccount)

--- AdvancedPython/oopprinciples/encapsulation.py
+++ AdvancedPython/oopprinciples/encapsulation.py
@@ -10,3 +10,21 @@ account = BankAccount("Jane", 1000)
 print(account.account_holder)
 account.balance += 500
 print(account.balance)
+
+# Example of Protected Attributes (BankAccount)
+class BankAccount:
+    def __init__(self, account_holder, balance):
+        self.account_holder = account_holder
+        self._balance = balance
+
+
+    def get_balance(self):
+        return self._balance
+
+
+class SavingsAccount(BankAccount):
+    def add_interest(self, interest_rate):
+        self._balance += self._balance * interest_rate
+savings = SavingsAccount("Bob", 2000)
+savings.add_interest(0.05)
+print(savings.get_balance())
