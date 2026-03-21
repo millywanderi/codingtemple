27613daad93adf817c6a1b4bc93acb07912319cc
millywanderi <wanderinyambura5@gmail.com>
2026-03-21 14:38:41 +0300

    Test my understanding of class properties by completing a small coding challenge.

--- AdvancedPython/oopfundamentals/challenge.py
+++ AdvancedPython/oopfundamentals/challenge.py
@@ -124,3 +124,21 @@ print(account.get_balance())
 print(account.deposit(500))
 print(account.withdraw(200))
 print(account.withdraw(2000))
+
+
+"""
+Create a class Student with an __init__ that takes name and grade, 
+and stores them as properties
+Create an object s1 with name "Anna" and grade "A"
+Print the grade of s1
+Change the grade of s1 to "B"
+Print the updated grade
+"""
+class Student:
+    def __init__(self, name, grade):
+        self.name = name
+        self.grade = grade
+s1 = Student("Anna", "A")
+print(s1.grade)
+s1.grade = "B"
+print(s1.grade)
