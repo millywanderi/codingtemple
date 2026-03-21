e17ce889ca9ee4a8b40685d6792cbbc0042f0cb3
millywanderi <wanderinyambura5@gmail.com>
2026-03-21 14:48:03 +0300

    Test my understanding of class methods by completing a small coding challenge

--- AdvancedPython/oopfundamentals/challenge.py
+++ AdvancedPython/oopfundamentals/challenge.py
@@ -142,3 +142,23 @@ s1 = Student("Anna", "A")
 print(s1.grade)
 s1.grade = "B"
 print(s1.grade)
+
+
+"""
+Create a class called Rectangle
+Add an __init__ method with width and height, and store them as properties
+Add a method called area that returns the width multiplied by the height
+Create an object r1 with width 5 and height 3
+Print the area of r1
+"""
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+
+    def area(self):
+        return self.width * self.height
+
+r1 = Rectangle(5, 3)
+print(r1.area())
