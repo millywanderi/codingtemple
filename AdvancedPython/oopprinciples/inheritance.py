77bbc419929db8944d5676fbfcef3bc83bd8e57f
millywanderi <wanderinyambura5@gmail.com>
2026-03-23 14:18:02 +0300

    modify our video game Character example so that the attack() method behaves differently for Warriors and Mages

--- AdvancedPython/oopprinciples/inheritance.py
+++ AdvancedPython/oopprinciples/inheritance.py
@@ -69,3 +69,31 @@ warrior.use_shield()
 mage.move()
 mage.attack()
 mage.cast_spell()
+
+# modify our video game Character example so that the attack() method 
+#behaves differently for Warriors and Mages
+class Character:
+    def __init__(self, name, health, attack_power):
+        self.name = name
+        self.health = health
+        self.attack_power = attack_power
+
+
+    def attack(self):
+        print(f"{self.name} attacks with {self.attack_power} power!")
+
+
+class Warrior(Character):
+    def attack(self):
+        print(f"{self.name} slashes with a sword, dealing with {self.attack_power} damage!")
+
+
+class Mage(Character):
+    def attack(self):
+        print(f"{self.name} casts a fireball, dealing {self.attack_power} damage!")
+
+warrior = Warrior("Conan", 100, 20)
+mage = Mage("Gandalf", 80, 25)
+
+warrior.attack()
+mage.attack()
