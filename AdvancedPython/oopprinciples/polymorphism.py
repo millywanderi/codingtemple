180f6fdbeb983c443dc02f035d0f9b321365c7f3
millywanderi <wanderinyambura5@gmail.com>
2026-03-23 15:51:21 +0300

    Create a class called Vehicle and make Car, Boat, Plane child classes of Vehicle

--- AdvancedPython/oopprinciples/polymorphism.py
+++ AdvancedPython/oopprinciples/polymorphism.py
@@ -44,6 +44,41 @@ plane = Plane("Boeing", "747")
 for x in (car, boat, plane):
     x.move()
 
+# Create a class called Vehicle and make Car, Boat, Plane child 
+#classes of Vehicle
+class Vehicle:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+
+    def move(self):
+        print("Move!")
+
+
+class Car(Vehicle):
+    pass
+
+
+class Boat(Vehicle):
+    def move(self):
+        print("Sail!")
+
+
+class Plane(Vehicle):
+    def move(self):
+        print("Fly!")
+
+
+car = Car("Ford", "Mustang")
+boat = Boat("Ibiza", "Touring 20")
+plane = Plane("Boeing", "747")
+
+for x in (car, boat, plane):
+    print(x.brand)
+    print(x.model)
+    x.move()
+
 # Polymorphism with Game Characters
 class Character:
     @abstractmethod
