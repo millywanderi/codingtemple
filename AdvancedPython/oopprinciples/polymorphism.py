69e6155f3253a94f04761999442532705b5ebd0e
millywanderi <wanderinyambura5@gmail.com>
2026-03-24 13:30:57 +0300

    Polymorphism in a Social Media Platform

--- AdvancedPython/oopprinciples/polymorphism.py
+++ AdvancedPython/oopprinciples/polymorphism.py
@@ -109,3 +109,38 @@ archer = Archer()
 perform_attack(warrior)
 perform_attack(mage)
 perform_attack(archer)
+
+# Polymorphism in a Social Media Platform
+class User:
+    def post_content(self):
+        print("Posting generic content.")
+
+
+class RegularUser(User):
+    def post_content(self):
+        print("Posting a photo as a regular user.")
+
+
+class Influencer(User):
+    def post_content(self):
+        print("Posting a sponsored video as an influencer.")
+
+
+class Brand(User):
+    def post_content(self):
+        print("Posting an ad as a brand.")
+
+
+def publish_post(user):
+    user.post_content()
+
+
+default_user = User()
+user1 = RegularUser()
+user2 = Influencer()
+user3 = Brand()
+
+publish_post(default_user)
+publish_post(user1)
+publish_post(user2)
+publish_post(user3)
