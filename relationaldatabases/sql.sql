0f4fd7aec8abd54fc6001fe55378eb3d77187970
millywanderi <wanderinyambura5@gmail.com>
2026-03-25 17:40:23 +0300

    created product table

--- relationaldatabases/sql.sql
+++ relationaldatabases/sql.sql
@@ -2,9 +2,16 @@ CREATE DATABASE myusers;
 USE myusers;
 
 -- Created users table
-CREATE TABLE USERS (
+CREATE TABLE IF NOT EXISTS users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Products table
+CREATE TABLE IF NOT EXISTS products (
+    product_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50),
+    price DECIMAL(10,2)
 );
