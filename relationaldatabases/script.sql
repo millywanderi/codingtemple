9d307438f876d6209aef2476bec655309c411b66
millywanderi <wanderinyambura5@gmail.com>
2026-03-25 17:14:26 +0300

    Created a database, used it, and created a table

---
+++ relationaldatabases/script.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE IF NOT EXISTS mydatabase;
+USE mydatabase;
+
+CREATE TABLE IF NOT EXISTS users(
+    user_id INT AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (user_id)
+);
+
+INSERT INTO users(username, email)
+VALUES
+('kylie', 'kylie@gmail.com'),
+('lyle', 'lyle@gmail.com');
+
+SELECT * FROM users;
