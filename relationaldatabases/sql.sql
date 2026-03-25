d8cf1f5b07749ef78cd1002eb1e24b1df3e8df3b
millywanderi <wanderinyambura5@gmail.com>
2026-03-25 17:35:34 +0300

    Create users table

---
+++ relationaldatabases/sql.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE myusers;
+USE myusers;
+
+-- Created users table
+CREATE TABLE USERS (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
