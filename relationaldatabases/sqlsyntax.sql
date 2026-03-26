94b700453906fbc7e8968f286a9cc7a008e67e68
millywanderi <wanderinyambura5@gmail.com>
2026-03-26 16:42:18 +0300

    Display the table results

--- relationaldatabases/sqlsyntax.sql
+++ relationaldatabases/sqlsyntax.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS userinfo (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL,
-    create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Modify the table and column
@@ -20,3 +20,6 @@ VALUES ("kylie_kinsley", "kylie@example.com");
 
 -- Update the table
 UPDATE userinfo SET email = "kinsley@eample.com" WHERE user_id = 1;
+
+-- Display the results
+SELECT * FROM userinfo;
