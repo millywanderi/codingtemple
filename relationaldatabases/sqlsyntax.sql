363acadd9c76cfe9490bad9ddd4ef1e5bb9bc4bd
millywanderi <wanderinyambura5@gmail.com>
2026-03-26 16:17:11 +0300

    Used CREATE to create a table

--- relationaldatabases/sqlsyntax.sql
+++ relationaldatabases/sqlsyntax.sql
@@ -2,3 +2,10 @@
 CREATE DATABASE IF NOT EXISTS myuserdatabase;
 USE myuserdatabase;
 
+-- Create a table
+CREATE TABLE IF NOT EXISTS userinfo (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
