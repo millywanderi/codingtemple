a7032040e4d09ee9600fc64f0f23faf13fcb97ee
millywanderi <wanderinyambura5@gmail.com>
2026-03-27 13:46:46 +0300

    The User Class (A Model Representing a Table)

--- relationaldatabases/orm/models.py
+++ relationaldatabases/orm/models.py
@@ -4,3 +4,11 @@
 class Base(DeclaritiveBase):
     pass
 
+
+# The User Class (A Model Representing a Table)
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30), nullable=False)
+    email: Mapped[str] = mapped_column(String(100))
