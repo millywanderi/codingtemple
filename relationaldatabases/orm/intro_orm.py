c582f47d172f0e20fd1c286e13a24d6c074f103d
millywanderi <wanderinyambura5@gmail.com>
2026-03-27 16:45:54 +0300

    Query Data (SQL SELECT)

--- relationaldatabases/orm/intro_orm.py
+++ relationaldatabases/orm/intro_orm.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from sqlalchemy import create_engine, String
+from sqlalchemy import create_engine, String, select
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, Session
 
 # create engine
@@ -22,6 +22,20 @@ Base.metadata.create_all(engine)
 
 # Insert data into the table
 session = Session(engine)
-new_user = User(name="John", email="john@example.com")
-session.add(new_user)
-session.commit()
+email_to_insert = "john@example.com"
+existing_user = session.execute(select(User).where(User.email == email_to_insert)).scalar_one_or_none()
+
+if not existing_user:
+    new_user = User(name="John", email="john@example.com")
+    session.add(new_user)
+    session.commit()
+    print("User added:", existing_user.name, existing_user.email)
+else:
+    print("User already exists:", existing_user.name, existing_user.email)
+
+# Query Data (SQL SELECT)
+query = select(User)
+users = session.execute(query).scalars().all()
+
+for user in users:
+    print(user.name, user.email)
