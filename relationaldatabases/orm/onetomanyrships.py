5b24fa9ccb1575dafd8ab64fde09bfd82a3a2346
millywanderi <wanderinyambura5@gmail.com>
2026-03-30 14:40:24 +0300

    Added a new user

--- relationaldatabases/orm/onetomanyrships.py
+++ relationaldatabases/orm/onetomanyrships.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 
 from sqlalchemy import create_engine, String, ForeignKey
-from sqlalchemy.orm import DeclarativeBase, Session, Mapped, 
-mapped_column, relationship
+from sqlalchemy.orm import DeclarativeBase, Session, Mapped, mapped_column, relationship
 from typing import List, Optional
 
 # A user can have multiple pets but each pet has only 1 owner
-engine = create_engine('mysql+mysqlconnector://root:ciku2015@localhost/mypetdatabase', echo=True)
+engine = create_engine('mysql+mysqlconnector://millie:ciku2015@localhost/intro_orm', echo=True)
 
 # Base class for models
 class Base(DeclarativeBase):
@@ -20,10 +19,10 @@ class User(Base):
     # columns
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(30))
-    fullname: MappedOptional[[str]] = mapped_column(String(200))
+    fullname: Mapped[Optional[str]] = mapped_column(String(200))
     
     # One-to-Many: User -> List of Pet objects
-    pets: Mapped[List["Pet"]] = relationship(back_populates="owner", cascade="all, delete_orphan")
+    pets: Mapped[List["Pet"]] = relationship(back_populates="owner", cascade="all, delete-orphan")
 
 
 # Pet model
@@ -32,7 +31,7 @@ class Pets(Base):
 
     # columns
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_colum(String(200))
+    name: Mapped[str] = mapped_column(String(200))
     animal: Mapped[str] = mapped_column(String(200))
     user_id: Mapped[int] = mapped_column(ForeignKey("user_account.id"))
 
@@ -41,3 +40,9 @@ class Pets(Base):
 
 # create tables
 Base.metadata.create_all(engine)
+
+session = Session(engine)
+
+new_user = User(name="Peter", fullname="Peter John")
+session.add(new_user)
+session.commit()
