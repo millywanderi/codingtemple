df22eab71b3afed6d2ea5db5b2b1bc41ddc26a05
millywanderi <wanderinyambura5@gmail.com>
2026-04-01 15:39:59 +0300

    Convert Python objects into JSON strings, and print the values

--- relationaldatabases/webfundamentals/basicjason.py
+++ relationaldatabases/webfundamentals/basicjason.py
@@ -40,3 +40,14 @@ person_json = {
 }
 data = json.dumps(person_json)
 print(data)
+
+# Convert Python objects into JSON strings, and print the values
+print(json.dumps({"name": "John", "age": 30}))
+print(json.dumps(["apple", "bananas"]))
+print(json.dumps(("apple", "bananas")))
+print(json.dumps("hello"))
+print(json.dumps(42))
+print(json.dumps(31.76))
+print(json.dumps(True))
+print(json.dumps(False))
+print(json.dumps(None))
