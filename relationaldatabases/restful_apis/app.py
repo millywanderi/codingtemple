4f3ae6de3e56b166c713f4034c5da528401b1854
millywanderi <wanderinyambura5@gmail.com>
2026-04-02 13:17:09 +0300

    configuring the Flask app and connecting it to MySQL

---
+++ relationaldatabases/restful_apis/app.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Configure the Flask app and connect it to MySQL
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from sqlalchemy.orm import DeclarativeBase
+import os
+
+
