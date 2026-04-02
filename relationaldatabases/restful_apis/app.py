528e562e0020ac17402e81fa0cc1c12262a47f3a
millywanderi <wanderinyambura5@gmail.com>
2026-04-02 13:33:54 +0300

    Initialize SQLAlchemy and Marshmallow

--- relationaldatabases/restful_apis/app.py
+++ relationaldatabases/restful_apis/app.py
@@ -4,7 +4,11 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, relationship
+from sqlalchemy import ForeignKey, Table, Column, String, Integer
+from marshmallow import ValidatorError
+from typing import List, Optional
+from __future__ import annotations
 import os
 
 # Initialize Flask app
@@ -19,4 +23,7 @@ class Base(DeclarativeBase):
     pass
 
 # Initialize SQLAlchemy and Marshmallow
-db = SQL
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+ma = Marshmallow(app)
+
