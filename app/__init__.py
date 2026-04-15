58a541a6d3c215c339a6424e557d85120367ff1d
millywanderi <wanderinyambura5@gmail.com>
2026-04-15 16:54:16 +0300

    Add extensions to app

--- app/__init__.py
+++ app/__init__.py
@@ -19,4 +19,10 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(f"config.{config_name}")
 
+    #Add extensions to app
+    db.init_app(app)
+    ma.init_app(app)
+    limiter.init_app(app)
+    cache.init_app(app)
+
 
