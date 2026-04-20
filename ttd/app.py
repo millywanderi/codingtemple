c5484a5a0236adb70c98e9c085f4a3e810aa5f63
millywanderi <wanderinyambura5@gmail.com>
2026-04-20 15:20:33 +0300

    clean up our solution

--- ttd/app.py
+++ ttd/app.py
@@ -7,10 +7,7 @@ app = Flask(__name__)
 @app.route('/sum', methods=['POST'])
 def sum():
     data = request.get_json()
-    num1 = data['num1']
-    num2 = data['num2']
-    result = num1 + num2
-    return jsonify({'result': result})
+    return jsonify({'result': data['num1'] + data['num2']})
 
 
 if __name__ == '__main__':
