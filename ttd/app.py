7d2448be33f1f83f08614b6a9f98cefa2d2ad579
millywanderi <wanderinyambura5@gmail.com>
2026-04-20 14:35:24 +0300

    create a simple sum two numbers endpoint logic

--- ttd/app.py
+++ ttd/app.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+
+@app.route('/sum', methods=['POST'])
+def sum():
+    data = request.get_json()
+    num1 = data['num1']
+    num2 = data['num2']
+    result = num1 + num2
+    return jsonify({'result': result})
 
 
 if __name__ == '__main__':
