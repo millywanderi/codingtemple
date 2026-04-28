5ba556f8be4ef8592ece7ab1279c0bd686088775
millywanderi <wanderinyambura5@gmail.com>
2026-04-28 13:24:48 +0300

    Created model

--- app/app.py
+++ app/app.py
@@ -8,10 +8,18 @@ import jwt
 db = SQLAlchemy()
 SECRETE_KEY = "secret"
 
-# Models
+# Model
 class Member(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     DOB = db.Column(db.Date, nullable=False)
     password = db.Column(db.String(100), nullable=False)
+
+# Token Helpers
+def encode_token(user_id, role):
+    payload = {
+        "user_id": user_id,
+        "role": role
+    }
+    return jwt.encode(payload, SECRET_KEY, algorithm="HS256")
