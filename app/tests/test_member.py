ad00464a052100500744cdf0d8ec094d22844d3a
millywanderi <wanderinyambura5@gmail.com>
2026-04-30 12:51:15 +0300

    Created a create member test

--- app/tests/test_member.py
+++ app/tests/test_member.py
@@ -11,3 +11,15 @@ class TestMember(unittest.TestCase):
             db.drop_all()
             db.create_all()
         self.client = self.app.test_cllient()
+
+    def test_create_member(self):
+        member_payload = {
+            "name": "Tom Jerry",
+            "email": "tomj@example.com",
+            "DOB": "1900-01-01",
+            "password": "123"
+        }
+
+        response = self.client.post('/members/', json=member_payload)
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json['name'], "Tom Jerry")
