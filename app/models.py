dad66c392ab39a5e05afad12c39db90dde27469a
millywanderi <wanderinyambura5@gmail.com>
2026-04-30 13:26:34 +0300

    Created model for the app

---
+++ app/models.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from flask_alchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Member(db.Model):
+    __tablename__ = "members"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    DOB = db.Column(db.Date, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f"<Member {self.email}>"
