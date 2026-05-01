b7aa867ff02723698e76edc022d45d904c90cbb2
millywanderi <wanderinyambura5@gmail.com>
2026-05-01 12:44:03 +0300

    Valid login member setup test

--- app/tests/test_member.py
+++ app/tests/test_member.py
@@ -2,7 +2,8 @@
 
 from app import create_app
 #from app.models import db
-from models import db
+from models import db, Member
+from datetime import datetime
 import unittest
 
 class TestMember(unittest.TestCase):
@@ -35,3 +36,17 @@ class TestMember(unittest.TestCase):
         response = self.client.post('/members/', json=member_payload)
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.json['email'], ['Missing data for required field'])
+
+
+class TestMember(unittest.Testcase):
+
+    def setUp(self):
+        self.app = create_app(TestingConfig)
+        self.member = Member(name="test_user", email="testuser@example.com", DOB=datetime.strptime("1900=-01-01", "%Y-%m-%d").date(), password='test')
+        with self.app.app_context():
+            db.drop_all()
+            db.create_all()
+            db.session.add(self.member)
+            db.session.commit()
+        self.token = encode_token(1, 'admin')
+        self.client = self.app.test_client()
